aa81bd7e42666fd90836ab60d0db61fe5a3174ac
Miguel Quispe <dev@miguelquispe.com>
2026-07-18 08:13:56 -0500

    build: terraform setup gcp

---
+++ main.tf
@@ -0,0 +1,35 @@
+# HCL: HashiCorp Configuration Language
+# GCP Configuration
+
+# terraform init
+# terraform fmt
+# terraform validate
+# terraform apply
+# terraform apply
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("gservice.json")
+  project     = "pulumi-gcp-501610"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+# resource "google_compute_network" "vpc_network" {
+#   name = "terraform-network"
+# }
+
+resource "google_storage_bucket" "udemy-iac-training" {
+  name          = "udemy-iac-training"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
