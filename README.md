300077da69c046942e1bf07c33593d5a739c148c
Miguel Quispe <dev@miguelquispe.com>
2026-07-18 10:14:47 -0500

    build: add terraform config and folder app

---
+++ README.md
@@ -0,0 +1,18 @@
+# ExchangeApp
+
+Angular App Demo deployed to Google Cloud Run.
+
+## Overview
+
+This repository is a technical demonstration focused on automating the deployment of web applications to GCP Cloud Run using Terraform as the primary Infrastructure as Code (IaC) tool.
+
+## Tech Stack
+
+- Frontend: Angular
+- Infrastructure as Code (IaC): Terraform
+- Infrastructure: GCP Cloud Run
+- CI/CD: GitHub Actions
+
+## Deployment
+
+This project is automatically deployed to Google Cloud Run upon every push, with all infrastructure resources managed and provisioned via Terraform.
---
+++ main.tf
@@ -0,0 +1,66 @@
+# HCL: HashiCorp Configuration Language
+# GCP Configuration
+
+# terraform init
+# terraform fmt
+# terraform validate
+# terraform apply
+# terraform apply
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "pulumi-gcp-501610"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+# Cloud Run
+# resource "google_cloud_run_service" "cloudrun-exchange-app" {
+#   name     = "cloudrun-exchange-app"
+#   location = "us-central1"
+
+#   template {
+#     spec {
+#       containers {
+#         image = "gcr.io/pulumi-gcp-501610/exchange-app-terraform:latest"
+#       }
+#     }
+#   }
+
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
+
+# resource "google_compute_network" "vpc_network" {
+#   name = "terraform-network"
+# }
+
+# resource "google_storage_bucket" "udemy-iac-training" {
+#   name          = "udemy-iac-training"
+#   location      = "EU"
+#   force_destroy = true
+
+#   uniform_bucket_level_access = true
+# }
+
+# terraform {
+#   cloud {
+#     organization = "udemy-iac-course"
+
+#     workspaces {
+#       name = "udemy-iac-course"
+#     }
+#   }
+# }
+
+
+# GCP_SECRETS_ACCESS_KEY
